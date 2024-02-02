932e754e05807597fdd00f72ba47120ce60b3101
Ismoh <12631485+Ismoh@users.noreply.github.com>
2024-02-02 14:58:14 +0100

    Refactor error handling code in enet.c and enet.h

--- enet.c
+++ enet.c
@@ -37,6 +37,40 @@
 #define ENET_DEBUG
 #include "enet.h"
 
+// https://stackoverflow.com/questions/1387064/how-to-get-the-error-message-from-the-error-code-returned-by-getlasterror/45565001#45565001
+#include <Windows.h>
+#include <system_error>
+#include <memory>
+typedef std::basic_string<TCHAR> String;
+
+String GetErrorMessage(DWORD dwErrorCode)
+{
+    LPTSTR psz{ nullptr };
+    const DWORD cchMsg = FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM
+                                         | FORMAT_MESSAGE_IGNORE_INSERTS
+                                         | FORMAT_MESSAGE_ALLOCATE_BUFFER,
+                                       NULL, // (not used with FORMAT_MESSAGE_FROM_SYSTEM)
+                                       dwErrorCode,
+                                       MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                                       reinterpret_cast<LPTSTR>(&psz),
+                                       0,
+                                       NULL);
+    if (cchMsg > 0)
+    {
+        // Assign buffer to smart pointer with custom deleter so that memory gets released
+        // in case String's c'tor throws an exception.
+        auto deleter = [](void* p) { ::LocalFree(p); };
+        std::unique_ptr<TCHAR, decltype(deleter)> ptrBuffer(psz, deleter);
+        return String(ptrBuffer.get(), cchMsg);
+    }
+    else
+    {
+        auto error_code{ ::GetLastError() };
+        throw std::system_error( error_code, std::system_category(),
+                                 "Failed to retrieve error message string.");
+    }
+}
+
 #define check_host(l, idx) \
 	*(ENetHost **)luaL_checkudata(l, idx, "enet_host")
 
--- enet.h
+++ enet.h
@@ -842,45 +842,6 @@ extern "C" {
         ENetPacket *  packet;    /**< packet associated with the event, if appropriate */
     } ENetEvent;
 
-
-// =======================================================================//
-// !
-// ! Better error handling
-// !
-// =======================================================================//
-// https://stackoverflow.com/questions/1387064/how-to-get-the-error-message-from-the-error-code-returned-by-getlasterror/45565001#45565001
-
-#include <string>
-typedef std::basic_string<TCHAR> String;
-
-String GetErrorMessage(DWORD dwErrorCode)
-{
-    LPTSTR psz{ nullptr };
-    const DWORD cchMsg = FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM
-                                         | FORMAT_MESSAGE_IGNORE_INSERTS
-                                         | FORMAT_MESSAGE_ALLOCATE_BUFFER,
-                                       NULL, // (not used with FORMAT_MESSAGE_FROM_SYSTEM)
-                                       dwErrorCode,
-                                       MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-                                       reinterpret_cast<LPTSTR>(&psz),
-                                       0,
-                                       NULL);
-    if (cchMsg > 0)
-    {
-        // Assign buffer to smart pointer with custom deleter so that memory gets released
-        // in case String's c'tor throws an exception.
-        auto deleter = [](void* p) { ::LocalFree(p); };
-        std::unique_ptr<TCHAR, decltype(deleter)> ptrBuffer(psz, deleter);
-        return String(ptrBuffer.get(), cchMsg);
-    }
-    else
-    {
-        auto error_code{ ::GetLastError() };
-        throw std::system_error( error_code, std::system_category(),
-                                 "Failed to retrieve error message string.");
-    }
-}
-
 // =======================================================================//
 // !
 // ! Public API
